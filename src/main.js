a045f1f8242bc9e34f4abe0faaef6a1a99f0a8d0
Shashikant <shashikant.tripathi187@gmail.com>
2025-04-02 23:23:46 +0530

    Updated Autofocus into a seperate file.

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router/index'
 import App from './App.vue'
+import Autofocus from './directives/vAutofocus'
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
 
-app.directive('autofocus', {
-  mounted: (el) => {
-    el.focus();
-  }
-});
+app.directive('autofocus', Autofocus);
 
-app.mount('#app')
+app.mount('#app');
