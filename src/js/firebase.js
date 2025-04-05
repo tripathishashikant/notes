a56dabe89fdc2c405daa9710c8c05bdb5a530fa3
Shashikant <shashikant.tripathi187@gmail.com>
2025-04-05 13:24:57 +0530

    feat - Issue #2: 1. Firebase setup completed.

---
+++ src/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPDN3j7llt0uXhaLMLXK9pDjQPUQXPMZ8",
+  authDomain: "notes-fa640.firebaseapp.com",
+  projectId: "notes-fa640",
+  storageBucket: "notes-fa640.firebasestorage.app",
+  messagingSenderId: "139907762131",
+  appId: "1:139907762131:web:99fe5fed3c388f93afd126"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
